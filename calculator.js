194b56ac557e3bd33ec442a95bfe3814eb817b31
chentiwuni <chienntiwuni@gmail.com>
2023-10-07 13:10:45 +0000

    working on an error

--- calculator.js
+++ calculator.js
@@ -1,20 +1,29 @@
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-}
-
-function clearDisplay() {
-    document.getElementById('display').value = "";
-}
-
 function computeDisplay() {
     try {
     const expression = document.getElementById('display').value;
     const result = eval(expression);
     document.getElementById('display').value = result;
+    return result;
     } catch(error) {
-        document.getElementById('display').value = "Error input";
+        document.getElementById('display').value = "Error input"
+        return "Error input";
 
     }
+}
+
+function appendToDisplay(value) {
+    const resultValue = computeDisplay();
+
+    if (resultValue !== undefined || resultValue !== null || resultValue !== "Error input") {
+        document.getElementById('display').value = value;
+    } else {
+        document.getElementById('display').value += value;
+    }
+    
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = "";
 }
 
 function deletOneValue(){
