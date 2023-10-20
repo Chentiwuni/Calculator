4a22eda2d63658618b53b2aba33db4bedd877b87
chentiwuni <chienntiwuni@gmail.com>
2023-10-20 22:09:24 +0000

    added comments to the js. file

--- calculator.js
+++ calculator.js
@@ -1,25 +1,32 @@
-let calculated = false;
-document.getElementById('expression-display').value = "0";
+let calculated = false; //check if function computeDisplay() has a value
+document.getElementById('expression-display').value = "0"; //set expression-display initial to 0
+
+//function to do the calculations on expression-display
 function computeDisplay() {
     try {
-    let expression = document.getElementById('expression-display').value;
-    expression = expression.replace('×', '*').replace('÷', '/');
+    let expression = document.getElementById('expression-display').value; //fetch expression
+
+    //replace x and ÷ back to * and / which is understood by math.evaluate method
+    expression = expression.replace('×', '*').replace('÷', '/'); 
     const result = math.evaluate(expression);
     document.getElementById('calculated-display').innerHTML = result;
-    calculated = true;
+    calculated = true; //set computeToDisplay() to having value
     } catch(error) {
         document.getElementById('calculated-display').innerHTML = "Error input"
-        calculated = true;
+        calculated = true; //set computeToDisplay() to having value
     }
 }
 
+//function to append values to the expression-display screen
 function appendToDisplay(value) {
+    //if computeToDisplay has a value, in this case true
     if (calculated) {
-        if (!isNaN(value || value == '.')) {
+        if (!isNaN(value || value == '.')) { //set to clear display if value is a number or .
         clearDisplay();
         }
-        calculated = false;
+        calculated = false; //set the clearing to exercute only for the initial input
 
+        //check if value is an operator, if true, append to the calculated value for further computation
         if (value === '/' || value === '*' || value === '+' || value === '-' || value === '%') {
             const output = document.getElementById('calculated-display').innerHTML;
             document.getElementById('expression-display').value = output;
@@ -27,18 +34,24 @@ function appendToDisplay(value) {
         }
     } 
     const expressionDisplay = document.getElementById('expression-display');
+
+    //check if expression-display is '0', if true append '.' to that instead of clear
     if (expressionDisplay.value === '0' && value !== '.') {
         document.getElementById('expression-display').value = "";
     }
+
+    //make expression-display user friendly by replacing '*' and '/' with 'x' and '÷'. this is to replaced back at computeDisplay()
     value = value.replace('*', '×').replace('/', '÷');
     document.getElementById('expression-display').value += value;
 }
 
+//function to clear 'expression-display' and set it to '0' and only clear 'calculatd-display'
 function clearDisplay() {
     document.getElementById('expression-display').value = "0";
     document.getElementById('calculated-display').innerHTML = "";
 }
 
+//function to delete the later input from 'expression-display'
 function deletOneValue(){
     const value = document.getElementById('expression-display').value;
     const deletedKey = value.slice(0, -1);
