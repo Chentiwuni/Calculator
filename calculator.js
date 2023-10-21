1100caa425d3321281c6e67226d62e8bd030abff
chentiwuni <chienntiwuni@gmail.com>
2023-10-21 23:32:32 +0000

    Fixed issue of result overlapping output display

--- calculator.js
+++ calculator.js
@@ -4,11 +4,33 @@ document.getElementById('expression-display').value = "0"; //set expression-disp
 //function to do the calculations on expression-display
 function computeDisplay() {
     try {
-    let expression = document.getElementById('expression-display').value; //fetch expression
+    const expression = document.getElementById('expression-display').value; //fetch expression
+    const maxDisplayLength = 12;
+    const maxDecimalPlace = 10;
+    let calculatedDisplay = document.getElementById('calculated-display'); //displays output results
+
+    //used replace() to replace '×' and '÷' with '*' and '/' and a regular expression to replace in all occurrances
     const expressionReplaced = expression.replace(/×/g, '*').replace(/÷/g, '/');
     const result = math.evaluate(expressionReplaced);
-    document.getElementById('calculated-display').innerHTML = result;
+    
+    let formatedResult = result.toString(); //set result to string to use the string length method
+
+    //check if result is an integer, if true, truncate else round off result to 10 decimal places
+    if (Number.isInteger(result)) {
+
+    if (formatedResult.length > maxDisplayLength) {
+        formatedResult = formatedResult.substr(0, maxDisplayLength) + '...';
+
+    }
+    
+} else {
+    
+    formatedResult = parseFloat(result.toFixed(maxDecimalPlace));
+}
+
+    calculatedDisplay.innerHTML = formatedResult;
     calculated = true; //set computeToDisplay() to having value
+
     } catch(error) {
         document.getElementById('calculated-display').innerHTML = "Error input"
         calculated = true; //set computeToDisplay() to having value
@@ -17,9 +39,10 @@ function computeDisplay() {
 
 //function to append values to the expression-display screen
 function appendToDisplay(value) {
+
     //if computeToDisplay has a value, in this case true
     if (calculated) {
-        if (!isNaN(value || value == '.')) { //set to clear display if value is a number or .
+        if (!isNaN(value) || value == '.') { //set to clear display if value is a number or '.'
         clearDisplay();
         }
         calculated = false; //set the clearing to exercute only for the initial input
